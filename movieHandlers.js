d2a17715eb060004bdc716f3e99ea18889ec44f7
yan05021 <justyiyan@gmail.com>
2023-05-22 17:14:02 +0200

    express-6

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,47 +1,111 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
+const database = require("./database");
 
 const getMovies = (req, res) => {
-  res.json(movies);
+    let sql = "select * from movies";
+    const sqlValues = [];
+
+    if (req.query.color != null) {
+        sql += " where color = ?";
+        sqlValues.push(req.query.color);
+
+        if (req.query.max_duration != null) {
+            sql += " and duration <= ?";
+            sqlValues.push(req.query.max_duration);
+        }
+    } else if (req.query.max_duration != null) {
+        sql += " where duration <= ?";
+        sqlValues.push(req.query.max_duration);
+    }
+
+    database
+        .query(sql, sqlValues)
+        .then(([movies]) => {
+            res.json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
 };
 
 const getMovieById = (req, res) => {
-  const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
+    database
+        .query("SELECT * FROM movies WHERE id=?", [id])
+        .then(([movies]) => {
+            if (movies[0] != null) {
+                res.json(movies[0]);
+            } else {
+                res.status(404).send("Not found");
+            }
+        })
+        .catch((err) => {
+            console.err(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
 
-  const movie = movies.find((movie) => movie.id === id);
+const postMovie = (req, res) => {
+    const { title, director, year, color, duration } = req.body;
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+    database
+        .query(
+            "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+            [title, director, year, color, duration]
+        )
+        .then(([result]) => {
+            res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the movie");
+        });
+};
+
+const updateMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+
+    database
+        .query(
+            "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+            [title, director, year, color, duration, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the movie");
+        });
+};
+
+const deleteMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("delete from movies where id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the movie");
+        });
 };
 
 module.exports = {
-  getMovies,
-  getMovieById,
+    getMovies,
+    getMovieById,
+    postMovie,
+    updateMovie,
+    deleteMovie,
 };
