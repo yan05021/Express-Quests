d7f5bebfae8c8657ddfe7ce56a4c90197fede778
yan05021 <justyiyan@gmail.com>
2023-05-23 10:02:56 +0200

    expresse-3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -12,9 +14,18 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
+const movieHandlers = require("./movieHandlers");
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.listen(port, (err) => {
     if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -1,8 +1,24 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
+    let sql = "select * from users";
+    const sqlValues = [];
+
+    if (req.query.language != null) {
+        sql += " where language  = ?";
+        sqlValues.push(req.query.language);
+
+        if (req.query.city != null) {
+            sql += " and city= ?";
+            sqlValues.push(req.query.city);
+        }
+    } else if (req.query.city != null) {
+        sql += "city= ?";
+        sqlValues.push(req.query.city);
+    }
+
     database
-        .query("select * from users")
+        .query(sql, sqlValues)
         .then(([users]) => {
             res.json(users);
         })
@@ -11,25 +27,84 @@ const getUsers = (req, res) => {
             res.status(500).send("Error retrieving data from database");
         });
 };
+
 const getUserById = (req, res) => {
     const id = parseInt(req.params.id);
-
     database
-        .query("select * from users where id = ?", [id])
+        .query("SELECT * FROM users WHERE id=?", [id])
         .then(([users]) => {
             if (users[0] != null) {
                 res.json(users[0]);
             } else {
+                res.status(404).send("Not found");
+            }
+        })
+        .catch((err) => {
+            console.err(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+    database
+        .query(
+            "INSERT INTO users(firstname, lastname, email, city, language ) VALUES (?, ?, ?, ?, ?)",
+            [firstname, lastname, email, city, language]
+        )
+        .then(([result]) => {
+            res.location(`/api/users/${result.insertId}`).sendStatus(201);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the movie");
+        });
+};
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+        .query(
+            "update users set firstname = ?, lastname = ?, email = ?,city = ?, language = ? where id = ?",
+            [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
                 res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
             }
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send("Error retrieving data from database");
+            res.status(500).send("Error editing the user");
+        });
+};
+
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("delete from users where id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the user");
         });
 };
 
 module.exports = {
     getUsers,
     getUserById,
+    postUser,
+    updateUser,
+    deleteUser,
 };
